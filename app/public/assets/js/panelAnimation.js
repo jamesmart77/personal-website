f533a9a73561a9a6877d3f7355eb6f2c7484b8bd
James Martineau <James.Martineau@libertymutual.com>
2017-12-30 12:08:55 -0500

    js animation sliding in swiper. background photo updated and link colors modified

--- app/public/assets/js/panelAnimation.js
+++ app/public/assets/js/panelAnimation.js
@@ -1,23 +1,59 @@
+var isFirstWheel = true;
+var widthAmt;
+var widthPercent;
+var intervalId;
+
 $(document).ready(function () {
     //initialize swiper when document ready
     var mySwiper = new Swiper('.swiper-container', {
         // Optional parameters
-            direction: 'vertical',
-            loop: true,
+        direction: 'vertical',
+        loop: true,
 
-            // If we need pagination
-            pagination: {
-                el: '.swiper-pagination',
-            },
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
 
-            mousewheel: {
-                sensitivity: 2,
-                eventsTarged: 'body'
-            }
+        mousewheel: {
+            sensitivity: 2,
+            eventsTarged: 'body'
+        }
     })
 });
 
 $("#main-row").on("wheel", function () {
-    $(".left-container").css("left", 0);
-    $(".swiper-container").css("left", 0);
+    if(isFirstWheel){
+        widthAmt = 0;
+        $(".swiper-container").fadeIn(3000, function(){
+            intervalId = setInterval(panelVisible, 2);
+        })
+    } else {
+        widthPercent = "35%";
+        panelAnimation();
+    }
 });
+
+
+function panelVisible() {
+    widthAmt++
+
+    if(widthAmt <= 35){
+        widthPercent = widthAmt + "%";
+        panelAnimation();
+    } else {
+        clearInterval(intervalId);
+        isFirstWheel = false;
+    }
+}
+
+function panelAnimation(){
+    $(".left-container").css({
+        "left": 0,
+        "width": widthPercent,
+    });
+    $(".swiper-container").css({
+        "left": 0,
+        "width": widthPercent,
+    });
+}
